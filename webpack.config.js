1eaccfebd1dc19ac0b3782d19a236df8dd4af09b
kimjuyeong <juyeong1260@gmailc.om>
2018-02-10 14:44:46 +0900

    webpack 환경설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,83 @@
-var path = require('path');
-var webpack = require('webpack');
-var html = require('html-webpack-plugin');
+/** dev */
 
+const webpack = require('webpack');
+const html = require('html-webpack-plugin');
+const path = require("path");
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const GenerateJsonPlugin = require('generate-json-webpack-plugin');
+const package = require("./package.json");
+const plugins = [
+    new GenerateJsonPlugin('version.json',
+        {
+            "version": package.version
+        }),
+    new webpack.DefinePlugin({
+        "process.env": {
+            NODE_ENV: JSON.stringify(NODE_ENV),
+            VERSION: JSON.stringify(package.version)
+        },
+    }),
+    new html({
+        title: 'ideaLRD',
+        description: 'use your brain',
+        template: './index.html',
+        favicon: './favicon.ico'
+    }),
+];
 module.exports = {
-    entry: ['./lib/index.js'],
+    entry: {
+        bundle: [
+            'babel-polyfill',
+            'core-js/es6',
+            'raf/polyfill',
+            'react-hot-loader/patch',
+            './lib/index.js'
+        ]
+    },
     output: {
-        path: path.resolve(__dirname, 'deploy'),
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: `deploy`,
         filename: 'bundle.js'
     },
+    resolve: {
+        modules: [
+            'node_modules'
+        ]
+    },
+    devtool: 'source-map',
+    devServer: {
+        port: 9000,
+        inline: true,
+        historyApiFallback: true,
+        hot: true,
+        contentBase: '.',
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Origin, X-Request-With, Content-Type, Accept, Key",
+        },
+    },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
-                exclude: /node_modules/,
                 query: {
-                    presets: ['react', 'env']
+                    presets: ['react', 'es2015', 'stage-2']
                 }
-            }
+            },
+            {
+                test: /\.(css|scss)$/,
+                loader: 'style-loader!css-loader!sass-loader'
+            },
+            {
+                test: /\.(png|jpeg|jpg|svg)$/,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf)$/,
+                loader: 'file-loader',
+            },
         ]
     },
-    devServer:{
-        port:9000,
-        contentBase: __dirname,
-        inline:true
-    },
-    devtool:'#inline-source-map'
+    plugins: plugins
 };
